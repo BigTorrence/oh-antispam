aabac0d4405820ce02ec55a59192df8cca8d4438
Asheesh Laroia <asheesh@asheesh.org>
2013-02-07 15:20:18 -0800

    Writing interactive tool to help

---
+++ wikispam/handle_spam.py
@@ -0,0 +1,72 @@
+#!/usr/bin/python
+import collections
+
+# FIXME: Automate the creation of this via the last run
+# e.g. git log --since='Wed Dec 5 22:57:06 2012 +0000'
+GIT_LOG_OUTPUT = open('/tmp/output').read()
+
+ParsedLogItem = collections.namedtuple('ParsedLogItem', 'author full_string')
+
+NextSteps = collections.namedtuple('NextSteps', 'people_to_ban')
+
+def split_into_lines(s):
+    ret = []
+    this_one = []
+    for line in s.split('\n'):
+        line = line.rstrip()
+        if line.startswith('commit'):
+            ret.append(this_one)
+            this_one = []
+        this_one.append(line)
+    return ret
+
+def parse(list_of_line_lists):
+    for line_list in list_of_line_lists:
+        yield parse_line_list(line_list)
+
+def parse_line_list(l):
+    full_string = '\n'.join(l)
+    author = None
+    for line in l:
+        if line.startswith("Author:"):
+            author = line.split()[1]
+    return ParsedLogItem(full_string=full_string, author=author)
+
+def list_authors(data):
+    for thing in sorted(set([datum.author for datum in data])):
+        print "*", thing
+
+def review_author(data):
+    print 'Here is a list of authors:'
+    list_authors(data)
+    name = raw_input('Who do you want to review?')
+    print name
+
+def quit_placeholder():
+    raise NotImplemented
+
+def prompt_for_action():
+    actions = {'a': (list_authors, 'List (a)uthors'),
+               'r': (review_author, '(R)eview author to see if all their edits are spam'),
+               'q': (quit_placeholder, '(Q)uit'),
+               }
+    msg = ' '.join([x[1] for x in actions.values()])
+    inp = raw_input("What action to take? " + msg).strip()
+    if (inp and inp[0] == 'q'):
+        return 'q'
+
+    # else...
+    action = actions.get(inp.strip(), None)
+    if action:
+        return action[0]
+    return None
+
+def process():
+    data = list(parse(
+            split_into_lines(GIT_LOG_OUTPUT)))
+    while True:
+        action = prompt_for_action()
+        if action:
+            if action == 'q':
+                return
+            action(data)
